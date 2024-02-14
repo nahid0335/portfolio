42388cb06e08c5f2395829b2a07fdaa2ca9ee47f
Tamzid hasan <nahid0335@gmail.com>
2024-02-14 15:42:33 +0900

    header complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,63 @@
+import { useState } from "react";
+
 const Header = () => {
+    const [brandName, setBrandName] = useState("Nahid Tamzid Hasan");
+    const [menuLinks, setMenuLinks] = useState([
+        {
+            title: "Home",
+            link: "/home",
+            id: 1,
+        },
+        {
+            title: "About",
+            link: "/about",
+            id: 2,
+        },
+        {
+            title: "Skills",
+            link: "/skills",
+            id: 3,
+        },
+        {
+            title: "Portfolio",
+            link: "/portfolio",
+            id: 4,
+        },
+        {
+            title: "Contact",
+            link: "/contact",
+            id: 5,
+        },
+    ]);
+    const [actionButton, setActionButton] = useState({
+        title: "Hire me",
+        link: "/hire-me",
+    });
+
     return (
         <>
-            <h1>website header</h1>
+            <div className="h-20 border main flex justify-between items-center px-16 bg-gray-100">
+                <div>
+                    {/* Logo*/}
+                    <h1 className="text-2xl font-bold">{brandName}</h1>
+                </div>
+
+                <div className="space-x-6">
+                    {/* menu links*/}
+                    {menuLinks.map((link) => (
+                        <a key={link.id} href={link.link} className=" hover:text-orange-600">
+                            {link.title}
+                        </a>
+                    ))}
+                </div>
+
+                <div>
+                    {/* buttons */}
+                    <a href={actionButton.link} className="px-4 py-2 bg-orange-500 shadow rounded-full text-1xl">
+                        {actionButton.title}
+                    </a>
+                </div>
+            </div>
         </>
     );
 }
