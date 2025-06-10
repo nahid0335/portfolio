ed3b4b9364d2cbd23a88a11e8e46449c7fa82837
nahid0335 <nahid0335@gmail.com>
2025-06-10 15:12:32 +0900

    new page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+//▼[change] 2025/06/10
+//import './index.css'
+import './assets/css/main.css';
+import './assets/css/normalize.min.css';
+import './assets/css/font-awesome.min.css';
+//▲[change] 2025/06/10
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
